ba559ab99c748dd813396127df0cca190dd0619d
mahmoud <mnaggar2006@gmail.com>
2024-04-05 23:04:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-console.log("hello");
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { google } = require("googleapis");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const youtube = google.youtube({
+  version: "v3",
+  auth: "AIzaSyA8lvRmiLfDY5bhjqYB0Uz4zvYVGpxloNM",
+});
+
+function getRandomVideo(channelId) {
+  return new Promise((resolve, reject) => {
+    youtube.search.list(
+      {
+        part: "snippet",
+        channelId: channelId,
+        maxResults: 50,
+        order: "date", // You can customize search parameters
+      },
+      (err, res) => {
+        if (err) {
+          console.error("Error fetching videos:", err);
+          reject(err);
+        } else {
+          const videos = res.data.items;
+          const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+          resolve(randomVideo);
+        }
+      },
+    );
+  });
+}
+
+client.once(Events.ClientReady, async (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  const youtubeChannels = ["UC5fDvuLnLPQcKiyHK6xKvZw", "UChbuH4HULlesX_rzlozkT6Q"];
+  client.channels.fetch("1225813992606531629").then((channel) => {
+    setInterval(async () => {
+      const date = new Date();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+      console.log(date.getHours())
+      if (hours == 9 && minutes == 0 && seconds == 0) {
+        const randomChannel = youtubeChannels[Math.floor(Math.random() * youtubeChannels.length)];
+        const video = await getRandomVideo(randomChannel);
+        client.users.send("1135502743289741416", `https://www.youtube.com/watch?v=${video.id.videoId}`);
+      }
+    }, 1000);
+  });
+});
+
+client.login(process.env.TOKEN);
